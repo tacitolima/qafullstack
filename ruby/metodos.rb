9f8683c7fbbb2b1e9a54523d7157b284bc810122
Tácito Lima <tacito.tp@gmail.com>
2018-12-21 23:35:52 -0200

    Metodos e operadores em ruby, curso

---
+++ ruby/metodos.rb
@@ -0,0 +1,18 @@
+def diga_ola(nome)
+    puts 'Olá,' + nome 
+end 
+
+diga_ola('Tácito')
+# def soma(v1, v2)
+#     total = v1 + v2
+#     total
+# end
+
+# puts 'insira o primeiro valor'
+# v1 = gets.chomp.to_i
+# puts 'insira o segundo valor'
+# v2 = gets.chomp.to_i
+
+# res = soma(v1,v2)
+
+# puts res
---
+++ ruby/operadores.rb
@@ -0,0 +1,40 @@
+
+# numero1 = 0
+# numero2 = 0
+# total = 0
+
+# puts 'Insira o primeiro número'
+# numero1 = gets.chomp.to_i
+
+# puts 'insira o segundo número'
+# numero2 = gets.chomp.to_i
+
+# total = numero1 + numero2
+# puts total
+
+#operadores de comparação
+
+v1 = 10
+v2 = 10
+puts v1 > v2
+
+#maior que >
+puts v1 > v2
+
+#menor que <
+puts v1 < v2
+
+#diferente que !=
+puts v1 != v2
+
+#maior ou igual >=
+puts v1 < v2
+
+#menor ou igual <=
+puts v1 < v2
+
+#igual ou igual ==
+puts v1 == v2
+
+#igual eqL?
+puts v1.eql?(v2)
